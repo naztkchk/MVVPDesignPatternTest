1cc4fca105a21c076b562e696c97e30e81653ba2
draxvel <ntv3331998@gmail.com>
2018-09-26 11:58:52 +0300

    Added View Model to MainActivity.

--- app/src/main/java/com/tkachuk/mvvpdesignpatterntest/MainActivity.kt
+++ app/src/main/java/com/tkachuk/mvvpdesignpatterntest/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.tkachuk.mvvpdesignpatterntest
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel::class.java)
+        noteViewModel.getAllNotes().observe(this, Observer {
+            //update recycler view
+        })
     }
 }
